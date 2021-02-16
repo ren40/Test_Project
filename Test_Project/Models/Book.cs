c851825a77a7c4cb19e5db97ed7875380a467977
ren40 <renat.1998.ru@gmail.com>
2021-02-16 22:47:28 +0300

    Добавьте файлы проекта.

---
+++ Test_Project/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test_Project.Models
+{
+    public class Book
+    {
+        // ID книги 
+        public int Id { get; set; }
+        // Название книги
+        public string Name { get; set; }
+        // Автор книги 
+        public string Author { get; set; }
+        // Цена 
+        public int Price { get; set; }
+
+    }
+}
